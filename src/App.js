a6c2712ecab912da0b07e03a00697f7af877a8d3
iwanjo <kamanuian@gmail.com>
2021-03-29 22:26:30 +0200

    Enabled ChatEngine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <ChatEngine
+      height = "100vh"
+      projectID="2d4178ac-96ca-499c-be69-910849c63e3c"
+      userName="iwanjo"
+      userSecret="12345"
+
+
+    />
+  )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// import './index.css';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from'./App';
