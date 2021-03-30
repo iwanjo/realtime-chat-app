054cafed8032baa272cda6316e9a471c1bb10a85
iwanjo <kamanuian@gmail.com>
2021-03-30 14:46:22 +0200

    user message templates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ const App = () => {
       projectID="2d4178ac-96ca-499c-be69-910849c63e3c"
       userName="iwanjo"
       userSecret="12345"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
 
 
     />
